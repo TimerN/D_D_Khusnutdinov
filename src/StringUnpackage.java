a60337c5e99a8f24a97af02db8ed50b815739108
TimerN <husnuttim@mail.ru>
2021-03-17 10:32:03 +0300

    Initial commit

---
+++ src/StringUnpackage.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class StringUnpackage {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        System.out.println(s);
+
+        //напишите тут ваш код
+    }
+}
