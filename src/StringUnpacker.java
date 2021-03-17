e60469b264e947426e67b37ba5b870745a7c3818
TimerN <husnuttim@mail.ru>
2021-03-17 11:37:45 +0300

    Unpacker

--- src/StringUnpacker.java
+++ src/StringUnpacker.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 
-public class StringUnpackage {
+public class StringUnpacker {
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
