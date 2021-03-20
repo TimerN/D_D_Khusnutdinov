c82f1e01da7feafe812adb0e6912f6100b6e37f9
TimerN <husnuttim@mail.ru>
2021-03-20 14:01:36 +0300

    comments and refact while to for

--- src/StringUnpacker.java
+++ src/StringUnpacker.java
@@ -7,35 +7,34 @@ public class StringUnpacker {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
-        int k = 0;
+        /* цикл для проверки строки на валидность */
         int leftBracketCounter = 0;
         int rightBrackerCounter = 0;
-        while (k < s.length()) {
+        for (int k = 0; k < s.length(); k++) {
             if (s.charAt(k) > 47 && s.charAt(k) < 58 || s.charAt(k) > 96 && s.charAt(k) < 123 || s.charAt(k) == '[' || s.charAt(k) == ']') {
-                if (s.charAt(k)=='[') {
+                if (s.charAt(k) == '[') {
                     leftBracketCounter++;
                 }
-                else if (s.charAt(k)==']') {
+                else if (s.charAt(k) == ']') {
                     rightBrackerCounter++;
                     if (leftBracketCounter < rightBrackerCounter) {
-                        System.out.println("String is not valid!2");
+                        System.out.println("String is not valid!1"); //есть уже закрывающая скобка хотя не было открывающей
                         break;
                     }
                 }
                 if (k == s.length() - 1) {
                     if (leftBracketCounter == rightBrackerCounter) {
+                /*строка валидна*/
                         Unpacker unpacker = new Unpacker(s);
                         System.out.println(unpacker.strUnpacked());
-                        System.out.println("Boom");
                     }
                     else {
-                        System.out.println("String is not valid!3");
+                        System.out.println("String is not valid!2"); //открывающих скобок больше
                     }
                 }
-                k++;
             }
             else {
-                System.out.println("String is not valid!4");
+                System.out.println("String is not valid!3"); //в строке есть другие символы кроме цифр, букв и скобок
                 break;
             }
         }
@@ -47,29 +46,25 @@ public class StringUnpacker {
             str = s;
         }
 
+        /* метод для распаковки строки без скобок*/
         public String strUnpacked() {
             String numbers = "";
             String symbols = "";
             String unpackedStr = "";
             int num = 1;
-            int i = 0;
-            while (i < str.length()) {
+            for (int i = 0; i < str.length(); i++) {
                 if (str.charAt(i) > 47 && str.charAt(i) < 58 && !(str.charAt(i + 1) > 47 && str.charAt(i + 1) < 58)) {
                     numbers = numbers + str.charAt(i);
                     num = Integer.parseInt(numbers);
-                    i++;
                 }
                 else if (str.charAt(i) > 47 && str.charAt(i) < 58) {
                     numbers = numbers + str.charAt(i);
-                    i++;
                 }
                 else if (str.charAt(i) > 96 && str.charAt(i) < 123 && i == str.length() - 1) {
                     symbols = symbols + str.charAt(i);
                     for (int j = 0; j < num; j++) {
                         unpackedStr = unpackedStr + symbols;
                     }
-
-                    i++;
                 }
                 else if (str.charAt(i) > 96 && str.charAt(i) < 123 && str.charAt(i+1) > 47 && str.charAt(i+1) < 58)  {
                     symbols = symbols + str.charAt(i);
@@ -78,19 +73,16 @@ public class StringUnpacker {
                     }
                     numbers = "";
                     symbols = "";
-                    i++;
                 }
                 else if (str.charAt(i) > 96 && str.charAt(i) < 123) {
                     symbols = symbols + str.charAt(i);
-                    i++;
                 }
                 else {
-                    unpackedStr = "String is not valid!1";
+                    unpackedStr = "Boom";
                     break;
                 }
             }
             return unpackedStr;
         }
     }
-
 }
